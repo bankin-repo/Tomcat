dcfbb6e500af14d1c3d6b5e3b587a8d187421586
ELPDevOps <139422116+ELPDevOps@users.noreply.github.com>
2023-07-26 19:25:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TOMCAT_HOST=192.168.1.2
-TOMCAT_USER=gamut-idc-node1
-TOMCAT_PWD=gamut
-TOMCAT_HOME=/home/gamut-idc-node1/Distros/apache-tomcat-8.5.11
+TOMCAT_HOST=54.226.165.119
+TOMCAT_USER=tomcat
+TOMCAT_PWD=12345678
+TOMCAT_HOME=/opt/apache-tomcat-9.0.78
 #
-WAR_FILE_PATH=target/gamutkart.war
+WAR_FILE_PATH=./target/ELPDevOps.war
 
 echo "Bringing tomcat server down for deployment..."
 sleep 3
@@ -14,9 +14,8 @@ $TOMCAT_HOME/bin/shutdown.sh 1>/dev/null 2>&1
 
 #1>file.log 2>&1
 if [ -f $WAR_FILE_PATH ];then
-    echo "deploying gamutkart application..."
+    echo "deploying ELPDevOps application..."
     sleep 4
-    sshpass -p "Gamut" ssh $TOMCAT_USER@$TOMCAT_HOST
     scp  $WAR_FILE_PATH $TOMCAT_USER@$TOMCAT_HOST:$TOMCAT_HOME/webapps
 else
    echo "war file doesn't exist! Please check the build"
